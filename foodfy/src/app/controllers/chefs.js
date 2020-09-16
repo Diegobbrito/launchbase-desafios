1cec842aa21de506f0dba77b050d691d516e97d2
Diego Brito <dibbrito@gmail.com>
2020-09-16 10:46:29 -0300

    chefs create update

--- foodfy/src/app/controllers/chefs.js
+++ foodfy/src/app/controllers/chefs.js
@@ -44,13 +44,21 @@ module.exports = {
     async post(request, response){
         const keys = Object.keys(request.body);
         const created_at = Date.now();
+
+        console.log(request.files)
         for (const key of keys) {
             if(request.body[key] == "")
             return response.send("Por favor, preencha todos os campos");
         }
 
-        if(request.files.length == 0)
-            return response.send('Por favor, envie uma imagem');  
+
+        if (!request.files) {
+            return response.status(400).send('No files were uploaded.');
+        }
+
+        // if(request.files.length == 0){
+        //     return response.send('Por favor, envie uma imagem');  
+        // }
 
         const filePromise =  request.files.map(file => File.create({
             ...file
